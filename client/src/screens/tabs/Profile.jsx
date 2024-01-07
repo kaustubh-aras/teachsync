6e8de8cbc8019ea02aea2cb7346691284d694de8
kaustubhkx <132165310+kaustubhkx@users.noreply.github.com>
2024-01-07 15:55:44 +0530

    Removed unused dependencies

--- client/src/screens/tabs/Profile.jsx
+++ client/src/screens/tabs/Profile.jsx
@@ -1,12 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View, Pressable} from 'react-native';
+import React from 'react';
+import UserAvatar from 'react-native-user-avatar';
+
 
 export default function Profile() {
   return (
     <View style={styles.container}>
-      <Text style={{color: 'black'}}>Profile</Text>
+      <UserAvatar size={100} name="Kaustubh Aras" src="https://dummyimage.com/100x100/000/fff" />
+      <Pressable style={styles.editProfileButton}>
+        <Text style={styles.editProfileButtonText}>Edit Profile</Text>
+      </Pressable>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -15,5 +20,21 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  editProfileButton: {
+    borderColor: 'black',
+    borderWidth: 1,
+    width: 100,
+    height: 25,
+    borderRadius: 5,
+    backgroundColor: 'white',
+  },
+  editProfileButtonText: {
+    color: 'black',
+    textAlign: 'center',
+    fontWeight: '400',
+    paddingTop:2,
+    paddingLeft: 10,
+    paddingRight: 10,
   },
 });
