dad9dee4e855aed51c5f6cb8808a607508804572
Kaustubh <kaustubhrx@gmail.com>
2024-03-01 19:08:35 +0530

    Add Monthly Report

--- client/src/screens/tabs/MonthlyReport.jsx
+++ client/src/screens/tabs/MonthlyReport.jsx
@@ -1,19 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  View,
+  TouchableOpacity,
+  Keyboard,
+} from 'react-native';
+import React, {useState} from 'react';
 
 export default function MonthlyReport() {
-  return (
-    <View style={styles.container}>
-      <Text style={{color:'white'}}>Monthly Report</Text>
-    </View>
-  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
